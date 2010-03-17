40f65f662ba7e1131a6dd9091a9a2d5b4bccd85e
karry.cz <karry.cz@6e4da178-2fb0-11df-9924-27d7866ff452>
2010-03-17 20:07:19 +0000

    add view to saved tracks and functionality for remove track

---
+++ trunk/app/assistants/app-assistant.js
@@ -0,0 +1,32 @@
+
+function AppAssistant ()
+{
+    // Empty
+}
+
+
+AppAssistant.prototype.setup = function()
+{
+     // Empty
+};
+
+
+/**
+ * Handles commands send from the app menu.
+ */
+AppAssistant.prototype.handleCommand = function(event)
+{
+
+    if (event.type == Mojo.Event.command)
+    {
+        switch (event.command)
+        {
+            case "about":
+                Mojo.Controller.stageController.pushScene("about");
+                break;
+            case "tracks":
+                Mojo.Controller.stageController.pushScene("tracks");
+                break;
+        }
+    }
+};
