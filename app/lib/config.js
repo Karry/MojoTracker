373e924fed39cf14bac0de487d76aa3eb486e7f7
karry.cz <karry.cz@6e4da178-2fb0-11df-9924-27d7866ff452>
2010-05-08 18:13:18 +0000

    add axis to graphs

--- app/lib/config.js
+++ app/lib/config.js
@@ -132,7 +132,7 @@ Config.prototype.userDistance = function(distanceM, canNegative){
 }
 
 Config.prototype.formatDateTime = function(dateobj){
-    // FIXME: add support locale, or configurable format
+    // FIXME: add support for locale, or configurable format
 	strRes = "NA";
 	secs = dateobj.getSeconds(); if (secs > 9) strSecs = String(secs); else strSecs = "0" + String(secs);
 	mins = dateobj.getMinutes(); if (mins > 9) strMins = String(mins); else strMins = "0" + String(mins);
@@ -142,6 +142,91 @@ Config.prototype.formatDateTime = function(dateobj){
 	yr   = dateobj.getFullYear(); strYr = String(yr);
     
     return strDays + "/" + strMnth + "/" + strYr + " " + strHrs + ":" + strMins + ":" + strSecs;    
+}
+
+Config.prototype.formatTime = function(dateobj, shortFormat){
+    // FIXME: add support for locale, or configurable format
+	strRes = "NA";
+	secs = dateobj.getSeconds(); if (secs > 9) strSecs = String(secs); else strSecs = "0" + String(secs);
+	mins = dateobj.getMinutes(); if (mins > 9) strMins = String(mins); else strMins = "0" + String(mins);
+	hrs  = dateobj.getHours(); if (hrs > 9) strHrs = String(hrs); else strHrs = "0" + String(hrs);
+    
+    return shortFormat? (strHrs + ":" + strMins) :
+            (strHrs + ":" + strMins + ":" + strSecs);
+}
+
+Config.prototype.generageXAxis = function( minTime, maxTime ){
+    result = new Array();
+    length = maxTime - minTime;
+    align = 5*60*1000; // 5 minutes
+    maxLines = 6;
+    if (length / align > maxLines) align =    10*60*1000;
+    if (length / align > maxLines) align =    15*60*1000;
+    if (length / align > maxLines) align =    30*60*1000;
+    if (length / align > maxLines) align = 1* 60*60*1000;
+    
+    dateobj = new Date(minTime);
+    startOfDay = Date.parse( dateobj.getFullYear() + '-' + dateobj.getMonth() + '-' + dateobj.getDate() + ' 0:00' );
+    alignedStart = minTime + ( align - ((minTime - startOfDay) % align));
+    
+    var i = 0;
+    for (time = alignedStart; time < maxTime ; time += align){
+        result[ i++ ] = {
+            time : time,
+            label : this.formatTime( new Date(time), true)
+        };  
+    }
+
+    return result; 
+}
+
+Config.prototype.generageYAxis = function(min, max, unitMultiply, unit){
+    result = new Array();
+    range = max - min;
+    align = 1 / unitMultiply;
+    maxLines = 9;
+    alignArr = new Array(2,5,10,20,25,50,100,150,200,250,500,1000);
+    for ( var i = 0 ; (i < alignArr.length) && (range / align > maxLines) ; i++){
+        align =  alignArr[i] / unitMultiply;
+    }
+
+    alignedStart = (min % align == 0)? min : min + ( align - (min % align));
+    var i = 0;
+    for (val = alignedStart; val < max ; val += align){
+        result[ i++ ] = {
+            value : val,
+            label : (val * unitMultiply).toFixed(0) // + unit
+        };  
+    }
+    
+    return result;
+}
+
+Config.prototype.generageAltitudeAxis = function(min, max){
+    unit = "m";
+    unitMultiply = 1;
+    
+    if (this.units == Config.IMPERIAL_UNITS){
+        unitMultiply = 3.2808;
+        unit = "ft";
+    }
+    return this.generageYAxis(min, max, unitMultiply, unit);
+}
+
+Config.prototype.generageSpeedAxis = function(min, max){
+    restVal = 1;
+    unit = "m/s";
+    
+    if (this.units == Config.IMPERIAL_UNITS){
+        unitMultiply = 2.237;
+        unit = "MPH";
+    }
+    if (this.units == Config.METRIC_UNITS){
+        unitMultiply = 3.6;
+        unit = "km/h";
+    }
+    
+    return this.generageYAxis(min, max, unitMultiply, unit);
 }
 
 Config.prototype.userDegree = function(degree){
