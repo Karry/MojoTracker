dac6473bbb587edd0b556114aa981a15a5fd4d89
karry.cz <karry.cz@6e4da178-2fb0-11df-9924-27d7866ff452>
2011-02-23 17:37:25 +0000

    add Ohloh.net widget to About scene

--- trunk/app/assistants/about-assistant.js
+++ trunk/app/assistants/about-assistant.js
@@ -1,14 +1,22 @@
 /**
  * ...constructor
 */
-function AboutAssistant()
-{
+function AboutAssistant(){
     
 }
-
 
 AboutAssistant.prototype.setup = function(){
     // Translate view
     $$(".i18n").each(function(e) { e.update($L(e.innerHTML)); });
+    
+    new Ajax.Request('http://www.ohloh.net/p/MojoTracker/widgets/project_users.html', {
+        onSuccess: function(response){
+            $('widget').innerHTML = response.responseText;
+            Mojo.log("we get '"+response.responseText+"'");
+        },
+        onFailure: function(e){
+            Mojo.Log.error("it fails "+e);
+        }
+    });
 }
 
